16cd2e7c4e97ff4d72f76a9a9c5fa9499ff222ae
jose51igual <avila51jose@gmail.com>
2024-10-10 13:01:57 +0200

    Main terminado

--- BatoiBooks/main.js
+++ BatoiBooks/main.js
@@ -1,5 +1,9 @@
 import './style.css'
 import batoiLogo from './public/logoBatoi.png'
+import data from './src/services/datos'
+import { averagePriceOfBooks, bookExists, booksCheeperThan, booksFromModule, booksFromUser, booksNotSold, booksOfTypeNotes, booksWithStatus, getBookById, getBookIndexById, getModuleByCode, getUserById, getUserByNickName, incrementPriceOfbooks } from './src/functions';
+
+
 
 
 document.querySelector('#app').innerHTML = `
@@ -13,3 +17,17 @@ document.querySelector('#app').innerHTML = `
     </p>
   </div>
 `
+console.log(getBookById(data.books, 1));
+console.log(getBookIndexById(data.books, 1));
+console.log(bookExists(data.books, 1));
+console.log(booksFromUser(data.books, 1));
+console.log(booksFromModule(data.books, 1));
+console.log(booksCheeperThan(data.books, 10));
+console.log(booksWithStatus(data.books, 'good'));
+console.log(averagePriceOfBooks(data.books));
+console.log(booksOfTypeNotes(data.books));
+console.log(booksNotSold(data.books));
+console.log(incrementPriceOfbooks(data.books, 0.1));
+console.log(getUserById(data.users, 1));
+console.log(getUserByNickName(data.users, 'Ximo'));
+console.log(getModuleByCode(data.modules, 'CV0001'));
